54b67c943452681003d1b41d5923d0396b7df619
Akromi Afif <socakromiafif@gmail.com>
2020-09-05 03:22:14 +0700

    test

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+
+
+var app = express();
+app.use(bodyParser.json());
+
+
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
+
